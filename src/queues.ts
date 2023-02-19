8fb95a7033bb84bdc0b1a38373edfed4b421df85
Jonathan Fallon <jonathan@scopopop.com>
2023-02-19 23:05:29 +0100

    use redis connection object

--- src/queues.ts
+++ src/queues.ts
@@ -1,17 +1,25 @@
-import Arena from "npm:bull-arena";
+// @deno-types="npm:@types/bull-arena#3.0.7"
+import Arena from "npm:bull-arena@3.30.4";
 import { Queue } from "npm:bullmq"
 
-const redis = { url: Deno.env.get('REDIS_URL') };
-const queues = (Deno.env.get("QUEUES") || '')
-  .split(',')
-  .map((s) => s.trim())
-  .sort()
-  .map(name => ({
-    name,
-    redis,
-    type: 'bullmq',
-    hostId: "worker",
-  }));
+const redis = {
+  port: parseInt(Deno.env.get('REDIS_PORT') || '10000'),
+  host: Deno.env.get('REDIS_HOST') || 'localhost',
+  password: Deno.env.get('REDIS_PASS'),
+  db: parseInt(Deno.env.get('REDIS_DB') || '0'),
+}
+
+const queues =
+  (Deno.env.get("QUEUES") || '')
+    .split(',')
+    .map((s) => s.trim())
+    .sort()
+    .map((name) => ({
+      name,
+      redis,
+      type: 'bullmq',
+      hostId: "worker",
+    }));
 
 export const arenaConfig = Arena({
   BullMQ: Queue,
